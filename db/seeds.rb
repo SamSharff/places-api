0df66b552a51ffdbb185c82b133bdef559595fcf
Sam Sharff <samanthaleec@gmail.com>
2022-04-13 08:25:47 -0400

    migrates database; seeds data successfully

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# place = Place.new(name: "Disney World", address: "Orlando, FL")
+# place.save
+
+# place = Place.new(name: "Funtown", address: "Scarborough, ME")
+# place.save
+
+# place = Place.new(name: "Splashtown", address: "Scarborough, ME")
+# place.save
+
+# place = Place.new(name: "Museum of Science", address: "Boston, MA")
+# place.save
+
+# place = Place.new(name: "Canobie Lake Park", address: "Salem, NH")
+# place.save
