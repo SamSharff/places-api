cb445c0f7b6600a53d0cf2b6416ed6c930d4e817
Sam Sharff <samanthaleec@gmail.com>
2022-04-13 08:35:03 -0400

    adds RESTful routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/places" => "places#index"
+  post "/places" => "places#create"
+  get "/place/:id" => "places#show"
+  patch "/place/:id" => "places#update"
+  delete "/places/:id" => "places#destroy"
 end
