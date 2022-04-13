785ac8525cc7234bf394d8b2e0c2993a12002e18
Sam Sharff <samanthaleec@gmail.com>
2022-04-13 08:55:29 -0400

    adds create action to places_controller.rb

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,15 +1,20 @@
 class PlacesController < ApplicationController
   def index
-  places = Place.all 
-  render json: places.as_json
+    places = Place.all 
+    render json: places.as_json
   end
 
+  def create
+    place = Place.new(
+      name: params[:name],
+      address: params[:address]
+    )
+    place.save
+    render json: place.as_json
+  end
 
-
-
-
-#create
-#show
-#update
-#destroy
-end
+  end
+  #create
+  #show
+  #update
+  #destroy
